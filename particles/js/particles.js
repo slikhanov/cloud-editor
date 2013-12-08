dda645c2708e0899c569116fa3d710d37e390d4b
Brrrutal <slikhan@gmail.com>
2013-12-08 12:31:51 +0200

    Clouds modeling capabilities.
    1. Added cloud particle picking.
    2. Made UI binding dynamic (bound to selected cloud particle).
    3. Exposed few test properties to tweak.
    4. Added few test textures.

--- particles/js/particles.js
+++ particles/js/particles.js
@@ -13,9 +13,9 @@ var settings, gui;
 function initGUI()
 {
     settings = new SceneSettings();
-    gui = new dat.GUI();
-    gui.add(settings, "scale").min(1.0).max(100.0).step(1.0)
-    gui.add(settings, "positionx").min(-10.0).max(10.0).step(1.0)
+    //gui = new dat.GUI();
+    //gui.add(settings, "scale").min(1.0).max(100.0).step(1.0)
+    //gui.add(settings, "positionx").min(-10.0).max(10.0).step(1.0)
 }
 
 
@@ -46,11 +46,29 @@ var billboard;
 var cloud;
 
 var cloudArray = [
-        [0, 1, 0, 1],
+       [[0, 0, 3, 0],
+        [1, 0, 0, 5],
+        [1, 0, 0, 1],
+        [1, 0, 3, 0],
+        [0, 4, 1, 0]],
+
+       [[0, 0, 1, 0],
+        [1, 3, 0, 1],
+        [2, 0, 0, 3],
         [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0]
+        [0, 5, 2, 0]],
+
+       [[0, 0, 3, 0],
+        [2, 4, 2, 1],
+        [4, 0, 0, 1],
+        [0, 1, 2, 1],
+        [0, 3, 0, 0]],
+
+       [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
 ];
 
 var controls;
@@ -64,23 +82,39 @@ function buildScene()
     controls.addEventListener( 'change', render );
 
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+    //scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+
+    var textures = 
+    [
+        THREE.ImageUtils.loadTexture("textures/001_ml.png"),
+        THREE.ImageUtils.loadTexture("textures/02_bPh.png"),
+        THREE.ImageUtils.loadTexture("textures/03_bPh.png"),
+        THREE.ImageUtils.loadTexture("textures/04_bPh.png"),
+        THREE.ImageUtils.loadTexture("textures/05_bPh.png")
+    ];
 
     cloud = new THREE.Object3D()
-    for (line = 0; line < cloudArray.length; line++)
+    for (layer = 0; layer < cloudArray.length; layer++)
+    for (line = 0; line < cloudArray[layer].length; line++)
     {
-        for (pos = 0; pos < cloudArray.length; pos++)
+        for (pos = 0; pos < cloudArray[layer][line].length; pos++)
         {
-            if (cloudArray[line][pos] == 0)
+            if (cloudArray[layer][line][pos] == 0)
                 continue;
 
             var planeGeometry = new THREE.PlaneGeometry(1.0, 1.0);
-            var planeMaterial = new THREE.MeshPhongMaterial( 
-            { ambient: 0x030303, color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading });
+            var planeMaterial = new THREE.MeshBasicMaterial( 
+            { color: 0xdddddd, shading: THREE.FlatShading, map: textures[cloudArray[layer][line][pos] - 1], transparent: true });
+            planeMaterial.depthWrite = false;
 
             var cell = new THREE.Mesh(planeGeometry, planeMaterial);
-            cell.position.setX(line);
-            cell.position.setY(pos);
+            cell.position.setX(line - cloudArray[layer].length * 0.5);
+            cell.position.setY(pos - cloudArray[layer][line].length * 0.5);
+            cell.position.setZ(layer);
+            cell.scale.set(1.5, 1.5, 1.5);
+            cell.grayness = Math.random();
+                
+
             cloud.add(cell);
         }
     }
@@ -88,9 +122,36 @@ function buildScene()
     scene.add(cloud);
 }
 
-initGUI();
+var gui2;// = new dat.GUI();
+
+function initGUI2()
+{
+    if (gui2 != null)
+        gui2.destroy();
+    gui2 = new dat.GUI();
+    gui2.add(settings, "scale").min(1.0).max(100.0).step(1.0)
+    if (selectedCloud != null)
+    {
+        // Scale.
+        var scaleFolder = gui2.addFolder("Particle Scale");
+        scaleFolder.add(selectedCloud.scale, "x").min(0.1).max(7.0).step(0.1)
+        scaleFolder.add(selectedCloud.scale, "y").min(0.1).max(7.0).step(0.1)
+        scaleFolder.add(selectedCloud.scale, "z").min(0.1).max(7.0).step(0.1)
+
+        // Position.
+        var positionFolder = gui2.addFolder("Particle Offset");
+        positionFolder.add(selectedCloud.position, "x").min(-3.0).max(3.0).step(0.1)
+        positionFolder.add(selectedCloud.position, "y").min(-3.0).max(3.0).step(0.1)
+        positionFolder.add(selectedCloud.position, "z").min(-3.0).max(3.0).step(0.1)
+    }
+}
+
 init();
+initGUI();
+initGUI2();
 animate();
+
+var projector;
 
 function init() 
 {
@@ -99,13 +160,17 @@ function init()
 
     buildScene();
 
+    projector = new THREE.Projector();
+
     renderer = new THREE.WebGLRenderer( { clearAlpha: 1 } );
+    renderer.setClearColor(0x000000, 1);
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
 
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     document.addEventListener('touchstart', onDocumentTouchStart, false);
     document.addEventListener('touchmove', onDocumentTouchMove, false);
+    document.addEventListener('click', onDocumentClick, false);
 
     window.addEventListener( 'resize', onWindowResize, false );
 }
@@ -119,6 +184,16 @@ function onWindowResize()
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
+
+function onDocumentClick(event) 
+{
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+    performSelection(event.clientX, event.clientY);
+}
+
+var testProjector = new THREE.Projector();
+var pickMouseVector = new THREE.Vector3();
 
 function onDocumentMouseMove(event) 
 {
@@ -146,19 +221,38 @@ function onDocumentTouchMove(event)
     }
 }
 
+//Selection support.
+
+var selectedCloud;
+
+function performSelection(x, y)
+{
+    pickMouseVector.x = 2 * (x / window.innerWidth) - 1;
+    pickMouseVector.y = 1 - 2 * (y / window.innerHeight );
+    
+    var testRaycaster = projector.pickingRay( pickMouseVector.clone(), camera );
+    var intersects = testRaycaster.intersectObjects( cloud.children );
+    if (intersects.length > 0)
+    {
+        selectedCloud = intersects[0].object;
+        initGUI2();
+    }
+}
+
 //
 
 function animate() 
 {
     requestAnimationFrame(animate);
     controls.update();
-    render();
+    //render();
 }
 
 function render() 
 {
     var time = Date.now() * 0.00005;
     cloud.scale.set(settings.scale, settings.scale, settings.scale);
+    cloud.children.forEach(function(entry){ entry.rotation.setFromRotationMatrix( camera.matrix );});
     renderer.render( scene, camera );
 }
 
